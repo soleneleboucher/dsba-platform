faae9dca35678a245040cf56124d3d056215555f
soleneleboucher <solene.leboucher@essec.edu>
2025-03-25 13:48:19 +0100

    Add a model and preprocessing steps

--- src/dsba/model_training.py
+++ src/dsba/model_training.py
@@ -10,6 +10,8 @@ import pandas as pd
 import xgboost as xgb
 from datetime import datetime
 from sklearn.base import ClassifierMixin, RegressorMixin
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 
 from dsba.model_registry import ClassifierMetadata
 from .preprocessing import split_features_and_target, preprocess_dataframe
@@ -35,3 +37,68 @@ def train_simple_classifier(
         performance_metrics={},
     )
     return model, metadata
+
+
+def train_logistic_regression(
+        df: pd.DataFrame, target_column: str, model_id: str
+) -> tuple[ClassifierMixin, ClassifierMetadata]:
+    logging.info("Start training a logistic regression")
+    df = preprocess_dataframe(df)
+    X, y = split_features_and_target(df, target_column)
+
+    param_grid = {
+        "C": [0.01, 0.1, 1, 10, 100],  
+        "max_iter": [1000, 2000, 5000]  
+    }
+
+    model = LogisticRegression(random_state=42)
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring="f1", n_jobs=-1)
+    grid_search.fit(X, y)
+
+    best_model = grid_search.best_estimator_
+    best_params = grid_search.best_params_
+
+    logging.info(f"Best hyperparameters found: {best_params}")
+
+    metadata = ClassifierMetadata(
+        id=model_id,
+        created_at=str(datetime.now()),
+        algorithm="logistic regression",
+        target_column=target_column,
+        hyperparameters=best_params,
+        description="Logistic Regression using GridSearchCV",
+        performance_metrics={},
+    )
+    return best_model, metadata
+
+
+"""
+def train_logistic_regression(
+        df: pd.DataFrame, target_column: str, model_id: str
+) -> tuple[ClassifierMixin, ClassifierMetadata]:
+    logging.info("Start training a logistic regression")
+    df = preprocess_dataframe(df)
+    X, y = split_features_and_target(df, target_column)
+
+    param_grid = {
+        "C": [0.01, 0.1, 1, 10],  
+        "max_iter": [1000]  
+    }
+
+    model = LogisticRegression(random_state=42, max_iter=1000)
+    model.fit(X, y)
+    
+
+    logging.info("Done training a simple classifier")
+
+    metadata = ClassifierMetadata(
+        id=model_id,
+        created_at=str(datetime.now()),
+        algorithm="logistic regression",
+        target_column=target_column,
+        hyperparameters={"random_state": 42},
+        description="Logistic Regression using GridSearchCV",
+        performance_metrics={},
+    )
+    return model, metadata
+"""
--- src/dsba/preprocessing.py
+++ src/dsba/preprocessing.py
@@ -1,6 +1,11 @@
 from pandas import DataFrame, Series
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns 
+import matplotlib.pyplot as plt
 
 # from sklearn.model_selection import train_test_split
 
@@ -34,3 +39,32 @@ def preprocess_dataframe(df):
         le = LabelEncoder()
         df[column] = le.fit_transform(df[column].astype(str))
     return df
+
+def fillna_and_standardize(df_train, df_test, target_column):
+    """
+    Replace NA values by the mean in numerical columns
+    Then standardize those values
+    """
+    X_train, y_train = split_features_and_target(df_train, target_column)
+    X_test, y_test = split_features_and_target(df_test, target_column)
+    numeric_columns = X_train.select_dtypes(include="number").columns
+    
+    imputer = SimpleImputer(strategy = "mean")
+    X_train[numeric_columns] = imputer.fit_transform(X_train[numeric_columns])
+    X_test[numeric_columns] = imputer.transform(X_test[numeric_columns])
+
+    scaler = StandardScaler()
+    X_train[numeric_columns] = scaler.fit_transform(X_train[numeric_columns])
+    X_test[numeric_columns] = scaler.transform(X_test[numeric_columns])
+
+    train = pd.concat([X_train, y_train], axis=1)
+    test = pd.concat([X_test, y_test], axis=1)
+        
+    return train, test
+
+
+def plot_correlation(df):
+    correlation_matrix = df.corr()
+    sns.heatmap(correlation_matrix, annot = True)
+    plt.title("Correlation Heatmap")
+    plt.show()
